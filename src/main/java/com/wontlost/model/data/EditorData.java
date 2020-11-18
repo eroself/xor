22d26bcba76f963c720291ee247ebee838d31be0
Ryan Pang <ryan.pang@live.com>
2020-11-18 20:31:27 +1300

    Update

--- src/main/java/com/wontlost/model/data/EditorData.java
+++ src/main/java/com/wontlost/model/data/EditorData.java
@@ -1,34 +1,8 @@
 package com.wontlost.model.data;
 
-import lombok.Data;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
-
-@Data
-@Document(collection = "xor")
 public class EditorData {
 
-    @Id
-    private String id;
-    private String editorId;
     private String editorContent;
-
-    public EditorData(){
-
-    }
-
-    public EditorData(String editorId, String editorContent){
-        this.editorId = editorId;
-        this.editorContent = editorContent;
-    }
-
-    public String getEditorId() {
-        return editorId;
-    }
-
-    public void setEditorId(String editorId) {
-        this.editorId = editorId;
-    }
 
     public String getEditorContent() {
         return editorContent;
